31aa487551b59738d824c194c82656a75f7c75e4
MinGuo <923049286@qq.com>
2016-11-15 00:14:15 +0800

    这是node练习

---
+++ node/pratice/event.js
@@ -0,0 +1,6 @@
+//引入events模块
+const events = requir('events');
+// 创建eventEmitter对象
+const eventEmitter = new eventEmitter();
+// 创建事件处理程序
+const connectHandler = conn
---
+++ node/pratice/server.js
@@ -0,0 +1,28 @@
+/*
+var http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    //发送响应内容
+    res.end('Hello Node.js!');
+}).listen(8888);
+
+console.log('响应结束');*/
+
+//官方实例
+const http = require('http');
+const hostname = 'localhost';
+const port = 3333;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Node.js');
+});
+
+//对此端口进行监听
+server.listen(port, hostname, () => {
+    console.log('我在监听服务器！')
+})
