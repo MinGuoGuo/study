41b3c6b315edc90fe5e8d6c0d0d1f03815b6994b
v_xiangmin <v_xiangmin@baidu.com>
2016-11-22 19:33:14 +0800

    白天练习

--- ant/nav_router_demo/src/components/Nav/Nav.js
+++ ant/nav_router_demo/src/components/Nav/Nav.js
@@ -1,79 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM, { render } from 'react-dom';
-import './index.css';
+import { Link } from 'react-router';
+import { IndexLink } from 'react-router';
+import './Nav.css';
 import { Menu, Icon } from 'antd';
 const SubMenu = Menu.SubMenu;
 
-/*
-const Sider = React.createClass({
-    getInitialState() {
-        return {
-            current: '1',
-            openKeys: [],
-        };
-    },
-    handleClick(e) {
-        console.log('Clicked: ', e);
-        this.setState({ current: e.key });
-    },
-    onOpenChange(openKeys) {
-        const state = this.state;
-        const latestOpenKey = openKeys.find(key => !(state.openKeys.indexOf(key) > -1));
-        const latestCloseKey = state.openKeys.find(key => !(openKeys.indexOf(key) > -1));
-
-        let nextOpenKeys = [];
-        if (latestOpenKey) {
-            nextOpenKeys = this.getAncestorKeys(latestOpenKey).concat(latestOpenKey);
-        }
-        if (latestCloseKey) {
-            nextOpenKeys = this.getAncestorKeys(latestCloseKey);
-        }
-        this.setState({ openKeys: nextOpenKeys });
-    },
-    getAncestorKeys(key) {
-        const map = {
-            sub3: ['sub2'],
-        };
-        return map[key] || [];
-    },
-    render() {
-        return (
-            <Menu
-                mode="inline"
-                openKeys={this.state.openKeys}
-                selectedKeys={[this.state.current]}
-                style={{ width: 240 }}
-                onOpenChange={this.onOpenChange}
-                onClick={this.handleClick}
-            >
-                <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                    <Menu.Item key="1">Option 1</Menu.Item>
-                    <Menu.Item key="2">Option 2</Menu.Item>
-                    <Menu.Item key="3">Option 3</Menu.Item>
-                    <Menu.Item key="4">Option 4</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
-                    <Menu.Item key="5">Option 5</Menu.Item>
-                    <Menu.Item key="6">Option 6</Menu.Item>
-                    <SubMenu key="sub3" title="Submenu">
-                        <Menu.Item key="7">Option 7</Menu.Item>
-                        <Menu.Item key="8">Option 8</Menu.Item>
-                    </SubMenu>
-                </SubMenu>
-                <SubMenu key="sub4" title={<span><Icon type="setting" /><span>Navigation Three</span></span>}>
-                    <Menu.Item key="9">Option 9</Menu.Item>
-                    <Menu.Item key="10">Option 10</Menu.Item>
-                    <Menu.Item key="11">Option 11</Menu.Item>
-                    <Menu.Item key="12">Option 12</Menu.Item>
-                </SubMenu>
-            </Menu>
-        );
-    },
-});
-ReactDOM.render(<Sider />, document.getElementById('root'));*/
-
-//ES写法
-class Sider extends Component {
+//ES6写法
+export default class Sider extends Component {
     constructor (props) {
         super(props)
         this.state = {
@@ -143,7 +77,3 @@ class Sider extends Component {
         );
     }
 }
-render(
-    <Sider />,
-    document.getElementById('root')
-);
