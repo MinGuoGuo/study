e60e31873dc0662d656aa7763d5134d3fee1017e
MinGuo <923049286@qq.com>
2016-12-19 23:51:28 +0800

    haha

--- practice/整屏滑动/index.js
+++ practice/整屏滑动/index.js
@@ -1,14 +1,27 @@
 // $(window).scroll( function() {
 //     console.log($(window).scrollTop());
 // });
-
-
 // 从第二个模块儿开始计算咯
 //console.log('firsr', $('.first').offset().top);
 // console.log('second', $('.second').offset().top);
 // console.log('scrollTop', $('.second').scrollTop());
 // console.log(111);
 // var top = $('.second').offset().top;
-$('.second').scroll(function () {
-    console.log(this.scrollTop());
+var isshow = $('.first');
+console.log($('.first').offset().top);
+console.log($('.second').offset().top);
+
+$(window).scroll(function() {
+    if($(this).scrollTop() <= isshow.offset().top) {
+        $('body').animate({
+            scrollTop: isshow.offset().top
+        }, 500);
+        isshow = isshow.next();
+    }
+    console.log(isshow);
+    console.log(isshow.offset().top);
+    // if(isshow.offset().top < $(this).scrollTop()) {
+    //     isshow = isshow.next();
+    //     $(this).scrollTop(isshow.offset().top);
+    // }
 })
