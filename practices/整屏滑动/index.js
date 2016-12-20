832982ec820c76c2e1e89fd5804dc63bc07fa202
MinGuo <923049286@qq.com>
2016-12-21 01:23:54 +0800

    改名了

---
+++ practices/整屏滑动/index.js
@@ -0,0 +1,69 @@
+var scrollFunc = function (e) {
+    var direct = 0;
+    e = e || window.event;
+
+    var thisDiv = $(e.target);
+    var winHeight = $(window).height();
+    var thisDivH = ($(window).height() - thisDiv.height())/2;
+
+    if (e.wheelDelta) {  //判断浏览器IE，谷歌滑轮事件
+        if (e.wheelDelta > 0) { //当滑轮向上滚动时
+            console.log("滑轮向上滚动");
+            thisDiv = thisDiv.prev();
+            if(thisDiv.length != 0) {
+                $('body').stop(true, false).animate({
+                    scrollTop: thisDiv.offset().top - thisDivH
+                }, 1000);
+                return false;
+            }else{
+                $('body').stop(false, true).animate({
+                    scrollTop: 0
+                }, 1000);
+                return false;
+            }
+        }
+        if (e.wheelDelta < 0) { //当滑轮向下滚动时
+            console.log("滑轮向下滚动");
+            thisDiv = thisDiv.next();
+            if(thisDiv.length != 0) {
+                $('body').stop(true, false).animate({
+                    scrollTop: thisDiv.offset().top - thisDivH
+                }, 1000);
+                return false;
+            }
+        }
+    } else if (e.detail) {  //Firefox滑轮事件
+        if (e.detail> 0) { //当滑轮向上滚动时
+            console.log("滑轮向上滚动");
+            thisDiv = thisDiv.prev();
+            if(thisDiv.length != 0) {
+                $('body').stop(true, false).animate({
+                    scrollTop: thisDiv.offset().top - thisDivH
+                }, 1000);
+                return false;
+            }else{
+                $('body').stop(true, false).animate({
+                    scrollTop: 0
+                }, 1000);
+                return false;
+            }
+        }
+        if (e.detail< 0) { //当滑轮向下滚动时
+            console.log("滑轮向下滚动");
+            thisDiv = thisDiv.next();
+            if(thisDiv.length != 0) {
+                $('body').stop(true, false).animate({
+                    scrollTop: thisDiv.offset().top - thisDivH
+                }, 1000);
+                return false;
+            }
+        }
+    }
+    // ScrollText(direct);
+}
+//给页面绑定滑轮滚动事件
+if (document.addEventListener) {
+    document.addEventListener('DOMMouseScroll', scrollFunc, false);
+}
+//滚动滑轮触发scrollFunc方法
+window.onmousewheel = document.onmousewheel = scrollFunc;
