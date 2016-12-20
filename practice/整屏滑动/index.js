6b1d46b0d75a78a3ed84a35443fc2e464c5ecec9
MinGuo <923049286@qq.com>
2016-12-20 22:42:02 +0800

    xiagaode

--- practice/整屏滑动/index.js
+++ practice/整屏滑动/index.js
@@ -8,20 +8,52 @@
 // console.log(111);
 // var top = $('.second').offset().top;
 var isshow = $('.first');
-console.log($('.first').offset().top);
-console.log($('.second').offset().top);
 
-$(window).scroll(function() {
-    if($(this).scrollTop() <= isshow.offset().top) {
-        $('body').animate({
-            scrollTop: isshow.offset().top
-        }, 500);
-        isshow = isshow.next();
+// $(window).scroll(function() {
+//     if($(this).is(':animated')) {
+//         return false;
+//     }
+//     console.log($(this).scrollTop());
+//     console.log(isshow.offset());
+//     if($(this).scrollTop() <= isshow.offset().top) {
+//         $('body').animate({
+//             scrollTop: isshow.offset().top
+//         }, 500);
+//         if(isshow.next().length == 0) {
+//             return false;
+//         }else{
+//             isshow = isshow.next();
+//         }
+//     }
+//
+//     // if(isshow.offset().top < $(this).scrollTop()) {
+//     //     isshow = isshow.next();
+//     //     $(this).scrollTop(isshow.offset().top);
+//     // }
+// })
+var scrollTop = $(this).scrollTop();
+$(window).on('mousewheel', function(e) {
+    if(!$('body').is(':animated')) {
+        var minus = $(this).scrollTop() - scrollTop;
+        console.log(minus);
+
+        var thisDiv = $(e.target);
+
+        if(minus > 0) {
+            thisDiv = thisDiv.next();
+        }else{
+            thisDiv = thisDiv.prev();
+        }
+
+        var winHeight = $(window).height();
+        var thisDivH = ($(window).height() - thisDiv.height())/2;
+
+        if(thisDiv.length != 0) {
+            $('body').animate({
+                scrollTop: thisDiv.offset().top - thisDivH
+            }, 1000);
+            return false;
+        }
+        scrollTop = $(this).scrollTop();
     }
-    console.log(isshow);
-    console.log(isshow.offset().top);
-    // if(isshow.offset().top < $(this).scrollTop()) {
-    //     isshow = isshow.next();
-    //     $(this).scrollTop(isshow.offset().top);
-    // }
-})
+});
