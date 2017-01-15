ce32a5069d38c4893e4f89f5cd59d246ec0f6925
MinGuo <923049286@qq.com>
2017-01-16 00:43:17 +0800

    asdf

---
+++ node/interface/pool/pool.js
@@ -0,0 +1,10 @@
+// 建立连接池；
+module.exports = {
+    students: {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'test',
+        port: 3306
+    }
+};
